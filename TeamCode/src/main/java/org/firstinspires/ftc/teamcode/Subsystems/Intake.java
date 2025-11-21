b3ffbe6b20eb5731004521067cd96d0f9c18ae1c
AtomicSky <sorenjones08@gmail.com>
2025-11-21 14:21:41 -0600

    Convert Intake to solvers
    Hi Shrey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -1,21 +1,20 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.impl.MotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 
-public class Intake implements Subsystem {
+public class Intake implements SubsystemBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
     public static final Intake X = new Intake();
     private final MotorEx motor = new MotorEx("intake");
-    private Intake(){}
-    public Command SpinOut(double pwr){
-        return new InstantCommand(() -> motor.setPower(pwr));
-    }
-    public Command SpinIn(double pwr){
-        return new InstantCommand(() -> motor.setPower(-pwr));
-    }
-    public void PwrOff(){
-        motor.setPower(0);
-    }
+    
+    public Intake(final HardwareMap hw, double pwr) {
+        X = hw;
+        Command SpinOut = new InstantCommand(() -> X.setPower(pwr));
+        Command SpinIn = new InstantCommand(() -> X.setPower(-pwr));
+        intakeCMD = new AdvancingCommand(SpinOut, SpinIn);
+  }
 }
