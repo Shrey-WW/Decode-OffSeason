443518a169475ae5b3d2c6783751f7610fef8286
Shrey-WW <Shrey.singh1236@gmail.com>
2026-02-02 18:37:47 -0600

    lut

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/VELOTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/VELOTesting.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Tests;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -38,43 +39,54 @@ public class VELOTesting extends CommandOpMode {
 
     @Override
     public void initialize(){
-        veloLUT.add(.28, .62);
-        veloLUT.add(.623, .53);
-        veloLUT.add(1.0,.48);
-        veloLUT.add(2.26, .42);
+        number = 0;
+        veloLUT.add(.18, .67);
+        veloLUT.add(.2, .6);
+        veloLUT.add(.305, .59);
+        veloLUT.add(.45, .5075);
+        veloLUT.add(.69, .475);
+        veloLUT.add(1.05, .4467);
+        veloLUT.add(1.61,.43);
+        veloLUT.add(2.81, .424);
+        veloLUT.add(5.5, .403);
         veloLUT.createLUT();
 
         transfer = new Transfer(hardwareMap);
         shooter = new Shooter(hardwareMap);
         intake = new Intake(hardwareMap);
-//        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-//        limelight.pipelineSwitch(0);
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
         imu = hardwareMap.get(IMU.class, "imu");
         RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP);
         imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-//        limelight.start();
+        LutMode=true;
+        limelight.start();
     }
 
     @Override
     public void run() {
         intake.Spin(1);
         transfer.Spin(1);
-        if (LutMode){
-            shooter.setTo(0);
+        if (limelight.getLatestResult().getTa()<.18) {
+            shooter.setTo(.51);
+
+        }
+        else if (LutMode){
+            shooter.setTo(veloLUT.get(limelight.getLatestResult().getTa()));
         }
         else {
             shooter.setTo(number);
         }
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-//        limelight.updateRobotOrientation(orientation.getYaw());
-//        LLResult llresult = limelight.getLatestResult();
+        limelight.updateRobotOrientation(orientation.getYaw());
+        LLResult llresult = limelight.getLatestResult();
 
-//        if (llresult != null && llresult.isValid()) {
-//            double Ta = llresult.getTa();
-//            telemetry.addData("Ta", Ta);
-//        }
+        if (llresult != null && llresult.isValid()) {
+            double Ta = llresult.getTa();
+            telemetry.addData("Ta", Ta);
+        }
 
         telemetry.addData("velo", shooter.getVelo());
         telemetry.update();
